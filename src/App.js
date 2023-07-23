82b2088f4287f48b5350c84b101ee09738b32b72
EduRossB <edu_barba@outlook.com>
2023-07-23 11:24:40 +0200

    visuales terminadas, empiezo con logica

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-import { FaArrowUp, FaArrowDown } from "react-icons/fa6";
+import { FaArrowUp, FaArrowDown, FaPlay, FaPause, FaRotate } from "react-icons/fa6";
 
 function App() {
+
+
+
+
+
+  
   return (
     <Row className="App">
       <Col xs={11} sm={10} md={8} lg={6} className="clock">
-        <header className="py-4">Reloj 25+5</header>
+        <header>Reloj 25+5</header>
         <main>
           <aside className="length-control w-100 d-flex flex-wrap justify-content-evenly">
             <article className="p-3">
@@ -14,13 +20,13 @@ function App() {
                 Duracion pausa
               </h1>
               <div className="d-flex col-12 align-items-baseline">
-                <Button className="length-control-elements">
+                <Button id="break-increment" className="length-control-elements button">
                   <FaArrowUp />
                 </Button>
                 <p id="break-length" className="length-control-elements fs-4">
                   35
                 </p>
-                <Button className="length-control-elements">
+                <Button id="break-decrement" className="length-control-elements button">
                   <FaArrowDown />
                 </Button>
               </div>
@@ -30,13 +36,13 @@ function App() {
                 Duracion pausa
               </h1>
               <div className="d-flex col-12 align-items-baseline">
-                <Button className="length-control-elements">
+                <Button id="session-increment" className="length-control-elements button">
                   <FaArrowUp />
                 </Button>
                 <p id="session-length" className="length-control-elements fs-4">
                   35
                 </p>
-                <Button className="length-control-elements">
+                <Button id="break-decrement" className="length-control-elements button">
                   <FaArrowDown />
                 </Button>
               </div>
@@ -49,6 +55,13 @@ function App() {
                 </div>
               </div>
             </Container>
+          </aside>
+          <aside className="w-100 d-flex flex-wrap justify-content-center">
+            <Button id="start_stop" className="button">
+            <FaPlay />
+            <FaPause />
+            </Button>
+            <Button className='button' id="reset"><FaRotate /></Button>
           </aside>
         </main>
         <footer></footer>
