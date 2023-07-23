7b602fdcbac58df451fdfcda76718e649c189c20
EduRossB <edu_barba@outlook.com>
2023-07-23 11:48:27 +0200

    agregue funcionalidad a botones de incremento y decremento, creacion de states y funcionalidad reset

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-import { FaArrowUp, FaArrowDown, FaPlay, FaPause, FaRotate } from "react-icons/fa6";
+import {
+  FaArrowUp,
+  FaArrowDown,
+  FaPlay,
+  FaPause,
+  FaRotate,
+} from "react-icons/fa6";
+import { useState } from "react";
 
 function App() {
+  const [breakTime, setBreakTime] = useState(5);
+  const [sessionTime, setSessionTime] = useState(25);
+  const [totalTime, setTotalTime] = useState(5);
 
+  const resetTimer = () => {
+    setBreakTime(5);
+    setSessionTime(25);
+  };
 
+  const changeTime = (id) => {
+    let newValue = 0;
+    switch (id) {
+      case "break-decrement":
+        newValue = breakTime - 1;
+        setBreakTime(newValue);
+        break;
+      case "break-increment":
+        newValue = breakTime + 1;
+        setBreakTime(newValue);
+        break;
+      case "session-decrement":
+        newValue = sessionTime - 1;
+        setSessionTime(newValue);
+        break;
+      case "session-increment":
+        newValue = sessionTime + 1;
+        setSessionTime(newValue);
+        break;
+      default:
+        console.log("Boton equivocado");
+    }
+  };
 
-
-
-  
   return (
     <Row className="App">
       <Col xs={11} sm={10} md={8} lg={6} className="clock">
@@ -20,29 +54,45 @@ function App() {
                 Duracion pausa
               </h1>
               <div className="d-flex col-12 align-items-baseline">
-                <Button id="break-increment" className="length-control-elements button">
+                <Button
+                  id="break-increment"
+                  onClick={(e) => changeTime(e.currentTarget.id)}
+                  className="length-control-elements button"
+                >
                   <FaArrowUp />
                 </Button>
                 <p id="break-length" className="length-control-elements fs-4">
-                  35
+                  {breakTime}
                 </p>
-                <Button id="break-decrement" className="length-control-elements button">
+                <Button
+                  id="break-decrement"
+                  onClick={(e) => changeTime(e.currentTarget.id)}
+                  className="length-control-elements button"
+                >
                   <FaArrowDown />
                 </Button>
               </div>
             </article>
             <article className="p-3">
               <h1 id="session-label" className="w-100 fs-3 col-12">
-                Duracion pausa
+                Duracion Sesion
               </h1>
               <div className="d-flex col-12 align-items-baseline">
-                <Button id="session-increment" className="length-control-elements button">
+                <Button
+                  id="session-increment"
+                  onClick={(e) => changeTime(e.currentTarget.id)}
+                  className="length-control-elements button"
+                >
                   <FaArrowUp />
                 </Button>
                 <p id="session-length" className="length-control-elements fs-4">
-                  35
+                  {sessionTime}
                 </p>
-                <Button id="break-decrement" className="length-control-elements button">
+                <Button
+                  id="session-decrement"
+                  onClick={(e) => changeTime(e.currentTarget.id)}
+                  className="length-control-elements button"
+                >
                   <FaArrowDown />
                 </Button>
               </div>
@@ -58,10 +108,12 @@ function App() {
           </aside>
           <aside className="w-100 d-flex flex-wrap justify-content-center">
             <Button id="start_stop" className="button">
-            <FaPlay />
-            <FaPause />
+              <FaPlay />
+              <FaPause />
             </Button>
-            <Button className='button' id="reset"><FaRotate /></Button>
+            <Button onClick={resetTimer} className="button" id="reset">
+              <FaRotate />
+            </Button>
           </aside>
         </main>
         <footer></footer>
